02f734a70c320d6583d5aa81f3eff4f6839ab593
Mike Lewis <mtlewis@users.noreply.github.com>
2021-10-27 15:56:58 +0100

    authz: fix type error in create-app
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>

--- packages/create-app/templates/default-app/packages/backend/src/types.ts
+++ packages/create-app/templates/default-app/packages/backend/src/types.ts
@@ -6,6 +6,7 @@ import {
   PluginEndpointDiscovery,
   UrlReader,
 } from '@backstage/backend-common';
+import { PermissionClient } from '@backstage/permission-common';
 
 export type PluginEnvironment = {
   logger: Logger;
@@ -14,4 +15,5 @@ export type PluginEnvironment = {
   config: Config;
   reader: UrlReader;
   discovery: PluginEndpointDiscovery;
+  permissions: PermissionClient;
 };
